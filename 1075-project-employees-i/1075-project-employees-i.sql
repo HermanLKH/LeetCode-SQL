f7e6398810583833880b17ac2c5f93044d1215a5
Herman King How LING <57675016+HermanLKH@users.noreply.github.com>
2026-09-26 21:23:22 +0800

    Time: 1503 ms (14.87%), Space: 0B (100%) - LeetHub

---
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -0,0 +1,9 @@
+/* Write your T-SQL query statement below */
+SELECT
+    p.project_id,
+    ROUND(AVG(CAST(COALESCE(e.experience_years, 0) AS DECIMAL(10, 2))), 2) AS average_years
+FROM Project p
+LEFT JOIN Employee e
+ON p.employee_id = e.employee_id
+GROUP BY p.project_id
+ORDER BY p.project_id
