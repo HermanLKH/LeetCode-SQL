39537899ad9ab009fed51da410727e3fc24f3d49
Herman King How LING <57675016+HermanLKH@users.noreply.github.com>
2026-09-26 21:22:07 +0800

    Time: 470 ms (9.15%), Space: 0B (100%) - LeetHub

---
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -0,0 +1,11 @@
+/* Write your T-SQL query statement below */
+SELECT
+    id,
+    movie,
+    description,
+    rating
+FROM Cinema
+WHERE
+    TRIM(description) <> 'boring' AND
+    id % 2 <> 0
+ORDER BY rating DESC
